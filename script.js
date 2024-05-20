aa65fb80f5a61943a6a60bee60cee886ef4a6402
Kateryna Oliynyk <catysmiler@gmail.com>
2024-05-20 16:47:49 +0300

    first commit

--- script.js
+++ script.js
@@ -1,62 +1,67 @@
-//задача1
-let styles = ["Jazz", "Blues"];
-console.log(styles);
-
-styles.push("Rock-n-Roll");
-console.log(styles);
-
-let middleIndex = Math.floor(styles.length / 2);
-styles[middleIndex] = "Classics";
-console.log(styles);
-
-let firstElement = styles.shift();
-console.log(firstElement);
-console.log(styles);
-
-styles.unshift("Rap", "Reggae");
-console.log(styles);
-
-
-//задача2
-function sumInput() {
-  let numbers = [];
-  while (true) {
-    let input = prompt("Введіть число", "");
-
-    if (input === null || input.trim() === "" || isNaN(input)) {
-      break;
-    }
-    numbers.push(Number(input));
-  }
-
-  let sum = 0;
-  for (let number of numbers) {
-    sum += number;
-  }
-
-  return sum;
+//завдання 1
+function unique(arr) {
+  return Array.from(new Set(arr));
 }
 
-let result = sumInput();
-alert("Сума чисел: " + result);
 
-//задача3
-function getMaxSubSum(arr) {
-  let maxSum = 0;
-  let currentSum = 0; // Поточна сума підмасиву
+let values = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+alert(unique(values));
 
-  for (let item of arr) {
-    currentSum += item;
-    if (currentSum > maxSum) {
-      maxSum = currentSum;
-    }
-    if (currentSum < 0) {
-      currentSum = 0;
+//завдання 2
+function aclean(arr) {
+  let anagrams = new Map();
+
+  for (let word of arr) {
+
+    let sortedWord = word.toLowerCase().split('').sort().join('');
+
+    anagrams.set(sortedWord, word);
+  }
+
+  return Array.from(anagrams.values());
+}
+
+let words = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+let uniqueWords = aclean(words);
+
+alert(uniqueWords);
+
+//завдання 3
+function countOccurrences(arr) {
+  let occurrences = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+    if (occurrences.has(item)) {
+      // Якщо елемент вже є
+      occurrences.set(item, occurrences.get(item) + 1);
+    } else {
+      // Якщо елемент ще не існує
+      occurrences.set(item, 1);
     }
   }
 
-  return maxSum;
+  return occurrences;
 }
 
-let arr = [1, -2, 3, 4, -9, 6];
-console.log(getMaxSubSum(arr)); 
+let num = [1, 2, 3, 4, 2, 1, 3, 5, 6, 5, 12];
+let result = countOccurrences(num);
+
+for (let [item, count] of result) {
+  console.log(`${item}: ${count} разів`);
+}
+
+//завдання 4
+function allUnique(arr) {
+  // Створюємо новий Set з елементами масиву
+  const uniqueElements = new Set(arr);
+  // Порівнюємо розмір Set з довжиною масиву
+  return uniqueElements.size === arr.length;
+}
+
+// Приклад використання:
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 2, 3, 4];
+
+console.log(allUnique(arr1)); // Виведе: true
+console.log(allUnique(arr2)); // Виведе: false
